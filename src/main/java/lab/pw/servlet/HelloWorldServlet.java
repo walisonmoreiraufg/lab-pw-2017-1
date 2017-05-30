a13586de62f67e5c20c0f8947033d3315e76767b
Walison Moreira <walison.moreira@gmail.com>
2017-05-30 19:51:09 -0300

    Servlet.

---
+++ src/main/java/lab/pw/servlet/HelloWorldServlet.java
@@ -0,0 +1,9 @@
+package lab.pw.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(value = "/hello")
+public class HelloWorldServlet extends HttpServlet {
+
+}
