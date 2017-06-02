51b7b2240e0121c898e1143420218af386e76804
Walison Moreira <walison.moreira@gmail.com>
2017-06-02 19:17:19 -0300

    Atualização do servlet.

--- src/main/java/lab/pw/servlet/HelloWorldServlet.java
+++ src/main/java/lab/pw/servlet/HelloWorldServlet.java
@@ -1,9 +1,35 @@
 package lab.pw.servlet;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(value = "/hello")
+@WebServlet(value = "/hello-servlet")
 public class HelloWorldServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(
+			HttpServletRequest req,
+			HttpServletResponse resp) throws ServletException, IOException {
 
+		String html = "<html>";
+		html += "<head>";
+		html += "<title>";
+		html += "Hello";
+		html += "</title>";
+		html += "</head>";
+		html += "<body>";
+		html += "<h1>";
+		html += "Hello";
+		html += "</h1>";
+		html += "</body>";
+		html += "</html>";
+		resp.getOutputStream().print(html);
+
+	}
+	
 }
